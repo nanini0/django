8aa1b4337c0e3c5a6dfffac4df11bd84ecff4fea
nanini0 <alvaro.robles15081999@gmail.com>
2025-04-19 15:19:08 -0400

    creando el primer html que muestra un queryset

--- blog/models.py
+++ blog/models.py
@@ -6,13 +6,14 @@ from django.utils import timezone,timesince
 
 
 class Posteo(models.Model):
-    auto = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    autor = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     titulo = models.CharField(max_length=200)
     texto = models.TextField()
     fecha_creacion = models.DateTimeField(default=timezone.now)
+    fecha_publicacion = models.DateTimeField(blank=True, null=True)
   
     def publicado(self):
-        self.fecha_creacion = timezone.now()
+        self.fecha_publicacion = timezone.now()
         self.save()
 
     def __str__(self):
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-
+from.models import Posteo
+from django.utils import timezone
 # Create your views here.
 
 
 def lista_posteo(request):
-    return render(request,'lista_posteo.html',{})
+    posts = Posteo.objects.filter(fecha_creacion__lt=timezone.now()).order_by('fecha_creacion')
+    return render(request, 'lista_posteo.html', {'posts': posts})
