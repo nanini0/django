ffb6e8e62a89a149224947e3035a08ea48d3775a
nanini0 <alvaro.robles15081999@gmail.com>
2025-04-20 09:27:25 -0400

    q

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns=[
     path('',views.lista_posteo,name='lista_posteo'),
+    path('post/<int:pk>/', views.post_detalle, name='post_detalle'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,14 @@
-from django.shortcuts import render
-from.models import Posteo
+from django.shortcuts import render, get_object_or_404
+from .models import Posteo
 from django.utils import timezone
+
 # Create your views here.
 
 
 def lista_posteo(request):
     posts = Posteo.objects.filter(fecha_creacion__lt=timezone.now()).order_by('fecha_creacion')
     return render(request, 'lista_posteo.html', {'posts': posts})
+
+def post_detalle(request,pk):
+    post = get_object_or_404(Posteo, pk=pk)
+    return render(request,'post_detalle.html',{'post':post})
